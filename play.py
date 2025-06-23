6284dea73ac20bed41ef48f4bd5d76f44dfc6f1d
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-23 15:59:46 +0200

    ease of use files

---
+++ play.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copytree('ToroGiallo_RP', 'C:/Users/mfede/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_resource_packs/ToroGiallo_RP', dirs_exist_ok=True)  
+shutil.copytree('ToroGiallo_BP', 'C:/Users/mfede/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_behavior_packs/ToroGiallo_BP', dirs_exist_ok=True)
