1351d68fc74949dc16da1c68e0810fad10213d10
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-29 00:13:05 +0200

    looked for documentation, testing and started the tnt launch mechanic

--- ToroGiallo_BP/scripts/main.js
+++ ToroGiallo_BP/scripts/main.js
@@ -1,1 +1,37 @@
-import {world, system} from "@minecraft/server";
+import {world, system, BlockPermutation} from "@minecraft/server";
+
+world.beforeEvents.playerInteractWithBlock.subscribe((event) => {
+    if (event.isFirstEvent) {
+        if (event.block.typeId === "tg:tnt_launcher" && !event.player.isSneaking) {
+            if (event.itemStack?.typeId === "minecraft:tnt") {
+                let amount = event.itemStack.amount;
+                const currentLoaded = event.block.permutation.getState("tg:loaded")
+                if (amount >= 1 && currentLoaded < 15) {
+                    
+                    const currentPerm = event.block.permutation;
+                    const newPerm = currentPerm.withState("tg:loaded", currentLoaded + 1);
+                    world.sendMessage(`Loading TNT: ${currentLoaded + 1}`);
+                    system.run(() => {
+                        event.block.setPermutation(newPerm);
+                    });
+                }
+            }
+            world.sendMessage(event.block.permutation.getState("tg:loaded").toString());
+            event.cancel = true; // Prevent further processing of the event
+        } else {
+            if (event.block.typeId === "tg:tnt_launcher") {
+                const currentPerm = event.block.permutation;
+                const newPerm = currentPerm.withState("tg:active", !currentPerm.getState("tg:active"));
+                system.run(() => {
+                    event.block.setPermutation(newPerm);
+                    world.sendMessage(`TNT Launcher is now ${newPerm.getState("tg:active") ? "active" : "inactive"}`);
+                    if (newPerm.getState("tg:active")) {
+                        //launchTNT(event.block);
+                        world.sendMessage(`${newPerm.getState("minecraft:cardinal_direction")}`);
+                    }
+                });
+
+            }
+        }
+    }
+});
--- play.py
+++ play.py
@@ -4,4 +4,4 @@ from subprocess import run
 shutil.copytree('ToroGiallo_RP', 'C:/Users/mfede/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_resource_packs/ToroGiallo_RP', dirs_exist_ok=True)  
 shutil.copytree('ToroGiallo_BP', 'C:/Users/mfede/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/development_behavior_packs/ToroGiallo_BP', dirs_exist_ok=True)
 
-run(["start", "", "C:/Users/mfede/OneDrive/Desktop/MinecraftB.lnk"], shell=True)
+#run(["start", "", "C:/Users/mfede/OneDrive/Desktop/MinecraftB.lnk"], shell=True)
